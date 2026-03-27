71804c3209f722033c6907d9f2810c8eec49ebdf
Gregory Dennis Putnam <213495108+mmmm013@users.noreply.github.com>
2026-03-27 01:35:21 -0500

    scaffold: add layout

---
+++ staging/gputnam-music-final-site/app/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, background: '#000', color: '#fff', fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
